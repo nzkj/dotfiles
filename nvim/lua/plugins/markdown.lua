085e1dd3fdb0ac0804b2654f2d87b11b8abc8a45
Keegan James <68798129+nzkj@users.noreply.github.com>
2024-03-16 14:57:39 +1300

    Add markdown previewer

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+
+  -- Markdown Previewer
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+
+}
