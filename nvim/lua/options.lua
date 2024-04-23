d9497e3020140cd739d5809e78cb1c77441a3f1c
Keegan James <68798129+nzkj@users.noreply.github.com>
2024-04-24 00:10:54 +1200

    improve completion keymaps

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -53,6 +53,8 @@ vim.o.relativenumber = true
 
 -- Enable GitHub copilot tab completion
 vim.g.copilot_assume_mapped = true
+vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
 
 -- Enable spell checking
 vim.o.spell = true
--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -37,29 +37,21 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ['<C-y>'] = cmp.mapping.confirm { select = true },
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
+          ['<C-l>'] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
-            else
-              fallback()
             end
           end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
+          ['<C-h>'] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
-            else
-              fallback()
             end
-          end, { 'i', 's' })
+          end, { 'i', 's' }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
