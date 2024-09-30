c5bae448ca04d86eeeda9037c5b82c58ab9724d5
nzkj <68798129+nzkj@users.noreply.github.com>
2024-09-30 22:26:16 +1300

    fix gitsigns in staged files

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -18,6 +18,13 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      signs_staged = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
       on_attach = function(bufnr)
         -- Navigating git hunks
         vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
